5ef966a00ccce455670cfba67ae030f2d656843c
Masaru Chida <mchida@cs.washington.edu>
2022-11-19 13:20:14 -0800

    first

--- src/main/java/main/Parser.java
+++ src/main/java/main/Parser.java
@@ -2,6 +2,10 @@ package main;
 
 import cse332.exceptions.NotYetImplementedException;
 
+import java.util.*;
+import java.util.LinkedList;
+import java.util.Arrays;
+
 public class Parser {
 
     /**
@@ -9,9 +13,42 @@ public class Parser {
      * @param adjMatrix Adjacency matrix
      * @return Adjacency list
      */
-    public static Object parse(int[][] adjMatrix) {
-        throw new NotYetImplementedException();
+    public static LinkedList<Integer>[] parse(int[][] adjMatrix) {
+    //    int l = adjMatrix[0].length;
+      //  ArrayList<ArrayList<Integer>> al = new ArrayList<>(l);
+
+       // for (int i=0; i < l; i++) {
+         //   al.add(new ArrayList<>());
+       // }
+
+      //  int i, j;
+       // for (i = 0; i < adjMatrix[0].length; i++) {
+         //   for (j = 0; j < adjMatrix.length; j++) {
+           //     if (adjMatrix[i][j] == 1) {
+             //       al.get(i).add(j);
+               // }
+            //}
+        //}
+
+        //return al;
+
+        int l = adjMatrix[0].length;
+        LinkedList<Integer>[] al = new LinkedList[l];
+        for (int i=0; i<l; i++) {
+            al[i]=new LinkedList<Integer>();
+        }
+        for (int i = 0; i < l; i++) {
+            for (int j = 0; j < adjMatrix.length; j++) {
+                if (adjMatrix[i][j] == 1) {
+                    al[i].add(j);
+                }
+            }
+        }
+
+        return al;
     }
+
+
 
     /**
      * Parse an adjacency matrix into an adjacency list with incoming edges instead of outgoing edges.
@@ -19,7 +56,40 @@ public class Parser {
      * @return Adjacency list with incoming edges
      */
     public static Object parseInverse(int[][] adjMatrix) {
-        throw new NotYetImplementedException();
-    }
+  //      int len = adjMatrix[0].length;
+    //    LinkedList[] link = new LinkedList[len];
 
+      //  int i, j;
+        //for (i = 0; i < adjMatrix[0].length; i++) {
+          //  for (j = 0; j < adjMatrix.length; j++) {
+            //    if (adjMatrix[i][j] == 1) {
+              //      link[j].add(i);
+               // }
+           // }
+        //}
+
+
+//        return link;
+
+        int l = adjMatrix.length;
+        LinkedList<Integer>[] al = (LinkedList<Integer>[]) new LinkedList[l];
+        for (int i = 0; i < adjMatrix[0].length; i++) {
+            for (int j = 0; j < l; j++) {
+                if (adjMatrix[i][j] == 1) {
+                    al[j].add(i);
+                }
+            }
+        }
+
+        return al;
+
+
+
+
+
+
+
+    }
 }
+
+
--- src/main/java/solvers/OutSequential.java
+++ src/main/java/solvers/OutSequential.java
@@ -3,15 +3,49 @@ package solvers;
 import cse332.exceptions.NotYetImplementedException;
 import cse332.interfaces.BellmanFordSolver;
 import main.Parser;
+import cse332.graph.GraphUtil;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class OutSequential implements BellmanFordSolver {
 
     public List<Integer> solve(int[][] adjMatrix, int source) {
-        Object g = Parser.parse(adjMatrix);
 
-        throw new NotYetImplementedException();
+        int l = adjMatrix[0].length;
+        LinkedList<Integer>[] adjList = Parser.parse(adjMatrix);
+        int[] D1 = new int[l];
+        int[] D2 = new int[l];
+        int[] P = new int[l];
+        //part 1
+
+        for (int i = 0; i < l; i++) {
+            D1[i] = Integer.MAX_VALUE;
+            D2[i] = D1[i];
+            P[i] = 0;
+        }
+        D1[source] = 0;
+        D2[source] = D1[source];
+
+        for (int i = 0; i < adjList.length; i++) {
+            int col = adjList[0].size();
+            int v = i;
+            D2[v] = D1[v];
+            for (int j = 0; j < col - 1; j++) {
+                int w = adjList[v].get(j);
+                int cost = w - v;
+                if (D1[w] > D2[v] + cost) {
+                    D1[w] = D2[v] + cost;
+                    P[w] = v;
+                }
+            }
+        }
+        //part3
+
+
+        LinkedList<Integer> cycle = (LinkedList<Integer>) GraphUtil.getCycle(P);
+        return cycle;
+
     }
-
 }
